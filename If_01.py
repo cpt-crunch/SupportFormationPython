42ff9c8ad112edc8b3f5efa89a0c8ed6c0cafa9d
Thierryptb <146729829+Thierryptb@users.noreply.github.com>
2023-10-11 15:36:02 +0200

    Create If_01.py

---
+++ If_01.py
@@ -0,0 +1,9 @@
+#Demonstration de la function 'if'
+
+a = 10 #Variable indiquant que 'a' est égale à '10'
+if a > 5: #On peut comparer l'attribut 'if' à l'attribut 'si'.
+	a = a + 1 #Donc si 'a' est supérieur à '5' on ajoute à la variable 'a' +1
+
+a = 3 #Variable indiquant que 'a' est égale à '3'
+if a > 5 
+	a = a + 1 #La variable est inférieur à 5 donc l'attribut 'if' ne s'applique pas
