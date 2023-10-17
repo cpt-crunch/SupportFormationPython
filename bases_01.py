2002bb73efa4032610e7728b795afe48c4de9cd1
Thierryptb <146729829+Thierryptb@users.noreply.github.com>
2023-10-17 14:08:30 +0200

    delete

--- bases_01.py
+++
@@ -1,7 +0,0 @@
-#Assign Variable
-name = 'Thierry' #Variable
-age = 25 #Variable
-address = 'FR' #Variable
-print(name) #Ecrire la variable name
-print(age) #Ecrire la variable age
-print(address) #Ecrire la variable address
